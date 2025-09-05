4994aa07820ede033a7d9761a5a312e841ae8534
mali204 <mali204@DUC-LAn20q3gaZ3.dedalus.lan>
2025-09-05 17:57:33 +0530

    Test 5 is committed by mairaj ali

--- TutorialsninjaProject/src/test/java/com/tutorialsninja/qa/testcases/LoginJava.java
+++ TutorialsninjaProject/src/test/java/com/tutorialsninja/qa/testcases/LoginJava.java
@@ -1,5 +1,7 @@
 package com.tutorialsninja.qa.testcases;
 
+import java.util.Date;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
@@ -9,7 +11,7 @@ public class LoginJava {
 
 	private FirefoxDriver driver;
 
-	@Test
+	@Test(priority = 1)
 
 	public void verifyLoginWithValidCredentials() {
 		driver = new FirefoxDriver();
@@ -22,14 +24,13 @@ public class LoginJava {
 		Assert.assertTrue(
 				driver.findElement(By.xpath("//a[normalize-space()='Edit your account information']")).isDisplayed());
 		Assert.assertTrue(driver.findElement(By.xpath("//a[normalize-space()='Account']")).isDisplayed());
-		String expectedValue="Account";
-		String actualValue=driver.findElement(By.xpath("//a[normalize-space()='Account']")).getText();
+		String expectedValue = "Account";
+		String actualValue = driver.findElement(By.xpath("//a[normalize-space()='Account']")).getText();
 		Assert.assertEquals(actualValue, expectedValue);
 		driver.quit();
-		}
-	
-	
-	@Test
+	}
+
+	@Test(priority = 2)
 
 	public void verifyLoginWithInValidCredentials() {
 		driver = new FirefoxDriver();
@@ -39,15 +40,49 @@ public class LoginJava {
 		driver.findElement(By.xpath("//input[@id='input-email']")).sendKeys("mairaj12345678123@gmail.com");
 		driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys("mairaj");
 		driver.findElement(By.xpath("//input[@value='Login']")).click();
-		String actualValue=driver.findElement(By.xpath("//div[contains(@class,'alert-dismissible')]")).getText();
-		String expectedValue="Warning: No match for E-Mail Address and/or Password.";
+		String actualValue = driver.findElement(By.xpath("//div[contains(@class,'alert-dismissible')]")).getText();
+		String expectedValue = "Warning: No match for E-Mail Address and/or Password.";
 		Assert.assertEquals(actualValue, expectedValue);
 		driver.quit();
-		}
+	}
 
-	
-	
+	@Test(priority = 3)
 
+	public void verifyLoginWithValidEMailAndWrongPassword() {
+		driver = new FirefoxDriver();
+		driver.get("https://tutorialsninja.com/demo/");
+		driver.findElement(By.linkText("My Account")).click();
+		driver.findElement(By.linkText("Login")).click();
+		driver.findElement(By.xpath("//input[@id='input-email']")).sendKeys("mairaj12345678@gmail.com");
+		driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys("mairaj12345");
+		driver.findElement(By.xpath("//input[@value='Login']")).click();
+		String actualValue = driver.findElement(By.xpath("//div[contains(@class,'alert-dismissible')]")).getText();
+		String expectedValue = "Warning: No match for E-Mail Address and/or Password.";
+		Assert.assertEquals(actualValue, expectedValue);
+		driver.quit();
+	}
+
+	@Test(priority = 4)
+
+	public void verifyLoginWithNoEMailNoPassword() {
+		driver = new FirefoxDriver();
+		driver.get("https://tutorialsninja.com/demo/");
+		driver.findElement(By.linkText("My Account")).click();
+		driver.findElement(By.linkText("Login")).click();
+		driver.findElement(By.xpath("//input[@id='input-email']")).sendKeys("");
+		driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys("");
+		driver.findElement(By.xpath("//input[@value='Login']")).click();
+		String actualValue = driver.findElement(By.xpath("//div[contains(@class,'alert-dismissible')]")).getText();
+		String expectedValue = "Warning: No match for E-Mail Address and/or Password.";
+		Assert.assertEquals(actualValue, expectedValue);
+		driver.quit();
+	}
 	
-	
+	@Test(priority = 5)
+	public void verifyTimeStampWithDate() {
+		Date date=new Date();
+		System.out.println(date.toString());
+		
+	}
+
 }
