515f7d952cf7410bc45f63fdf6808562b728d6a6
mali204 <mali204@DUC-LAn20q3gaZ3.dedalus.lan>
2025-09-09 18:39:18 +0530

    TC3 named SearchProduct is committed by mairaj ali dated 9/9/25

---
+++ TutorialsninjaProject/src/test/java/com/tutorialsninja/qa/testcases/SearchProduct.java
@@ -0,0 +1,48 @@
+package com.tutorialsninja.qa.testcases;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.tutorialsninja.qa.utility.Base;
+
+public class SearchProduct extends Base {
+
+	@BeforeMethod
+
+	public void setup() {
+		openBrowserApplication();
+	}
+
+	@Test(priority = 1)
+
+	void serachValidProduct() {
+		driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys("HP");
+		driver.findElement(By.xpath("//button[@class='btn btn-default btn-lg']")).click();
+		String actaulProductName=driver.findElement(By.xpath("//a[normalize-space()='HP LP3065']")).getText();
+		String expectedProductName="HP LP3065";
+		Assert.assertEquals(actaulProductName, expectedProductName);
+	}
+	
+	@Test(priority = 2)
+
+	void serachInValidProduct() {
+		driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys("HP123");
+		driver.findElement(By.xpath("//button[@class='btn btn-default btn-lg']")).click();
+		String actaulProductName=driver.findElement(By.xpath("//p[contains(text(),'There is no product that matches the search criter')]")).getText();
+		String expectedProductName="There is no product that matches the search criteria.";
+		Assert.assertEquals(actaulProductName, expectedProductName);
+	}
+
+
+	@AfterMethod
+
+	public void terDOwn() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
+}
